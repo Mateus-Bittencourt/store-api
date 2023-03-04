319d33827a789c239d0ecb161ccc104b964f563b
Mateus Bittencourt <mateusmbittencourt@gmail.com>
2023-03-04 18:56:59 -0300

    feat: add models

--- repositories/db.js
+++ repositories/db.js
@@ -1,19 +1,32 @@
-import pg from "pg";
+// import pg from "pg";
+import { Sequelize } from "sequelize";
 
-const connect = async () => {
-  if (global.connection) {
-    return global.connection.connect();
+// const connect = async () => {
+//   if (global.connection) {
+//     return global.connection.connect();
+//   }
+
+//   const pool = new pg.Pool({
+//     connectionString:
+//       "postgres://oodshszh:C8x6k2Fg-53285EJuHoF1kEViHRi5Cwx@drona.db.elephantsql.com/oodshszh",
+//   });
+//   global.connection = pool;
+
+//   return pool.connect();
+// };
+
+const sequelize = new Sequelize(
+  "postgres://oodshszh:C8x6k2Fg-53285EJuHoF1kEViHRi5Cwx@drona.db.elephantsql.com/oodshszh",
+  {
+    dialect: "postgres",
+    define: {
+      timestamps: false,
+    },
   }
+);
 
-  const pool = new pg.Pool({
-    connectionString:
-      "postgres://oodshszh:C8x6k2Fg-53285EJuHoF1kEViHRi5Cwx@drona.db.elephantsql.com/oodshszh",
-  });
-  global.connection = pool;
+export default sequelize;
 
-  return pool.connect();
-};
-
-export default {
-  connect,
-};
+// {
+//   // connect,
+// };
